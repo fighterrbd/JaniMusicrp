35f01a4fe87de3a1f447821cc6ad9e9961bfb98b
fighterrbd <fighterrbd@gmail.com>
2025-07-02 21:52:34 +0530

    Update play.py

--- JaniMusic/utils/inline/play.py
+++ JaniMusic/utils/inline/play.py
@@ -54,11 +54,11 @@ def stream_markup_timer(_, chat_id, played, dur):
     else:
         bar = "-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·"
     buttons = [
-                [
-            InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
-            )
+              #  [
+           # InlineKeyboardButton(
+               # text=f"{played} {bar} {dur}",
+               # callback_data="GetTimer",
+           # )
         ],
                 [
          InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
