731e7d44b67af50fd4b777c6367a686ece0a29b5
fighterrbd <fighterrbd@gmail.com>
2025-07-02 21:19:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+AaI_GATiYwQ5NjU1")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+AaI_GATiYwQ5NjU1")
 
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') 
-API_KEY = getenv("API_KEY", '30DxNexGenBots667d40')
+API_KEY = getenv("API_KEY", 'NxGBNexGenBots41dd4c')
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -55,7 +55,7 @@ SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
 
 
 # Maximum limit for fetching playlist's track from youtube, spotify, apple links.
-PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 5000))
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 50))
 
 
 # Telegram audio and video file size limit (in bytes)
@@ -81,21 +81,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/u15ml3.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/m1qze9.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/u15ml3.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/m1qze9.jpg"
 )
-PLAYLIST_IMG_URL = "https://files.catbox.moe/zqpsp8.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/zqpsp8.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/zqpsp8.jpg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/zqpsp8.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/zqpsp8.jpg"
-SOUNCLOUD_IMG_URL = "https://files.catbox.moe/zqpsp8.jpg"
-YOUTUBE_IMG_URL = "https://files.catbox.moe/zqpsp8.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/zqpsp8.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/zqpsp8.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/zqpsp8.jpg"
+PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
+STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
+TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
+SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
+YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
 
 
 def time_to_seconds(time):
